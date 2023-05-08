2b29bcfde47de9b0eccf14788da2fb7688811fc6
yashxmittal <mittal.yash3333@gmail.com>
2023-05-08 13:43:32 +0530

    feat: formating

--- src/controller/otp.controller.js
+++ src/controller/otp.controller.js
@@ -2,24 +2,25 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/user.model');
 const { twilio, verify } = require('../sms-service/sms.service');
 const tokenModel = require('../models/token.model');
-exports.findOne = async function(req, res){
-    try{
-        const projection = { _id: 0, __v:0}
+exports.findOne = async function (req, res) {
+    try {
+        const projection = { _id: 0, __v: 0 }
         const phoneNumber = req.body.phoneNumber;
-        const isUserPresent = await User.findOne({"phoneNumber" : phoneNumber},projection);
-        if(isUserPresent){
-            const token = await tokenModel.findOne({"key" : phoneNumber},projection);
-            if(token){
+        const isUserPresent = await User.findOne({ "phoneNumber": phoneNumber }, projection);
+        if (isUserPresent) {
+            const token = await tokenModel.findOne({ "key": phoneNumber }, projection);
+            if (token) {
                 const verification = await jwt.verify(token.token, "JWT_SECRET");
-                if(verification){
+                if (verification) {
                     const userName = isUserPresent.name;
                     res.status(200).json({
                         type: "Success",
-                        message: `WELCOME! ${userName} ` 
+                        message: `WELCOME! ${userName} `
                     })
-                }else{
+                } else {
+                    // TO DO make this function DRY(Don't repeat youself)
                     const result = await twilio(phoneNumber)
-                    if(result == 'pending'){
+                    if (result == 'pending') {
                         res.status(200).json({
                             type: "Success",
                             message: `OTP has been sent on number ${phoneNumber}`,
@@ -33,10 +34,24 @@ exports.findOne = async function(req, res){
                     }
                 }
             }
-
-        else{
+            else {
+                const result = await twilio(phoneNumber)
+                if (result == 'pending') {
+                    res.status(200).json({
+                        type: "Success",
+                        message: `OTP has been sent on number ${phoneNumber}`,
+                        redirecting: "redirect to enter otp api to enter and verify otp </otp/verify>"
+                    })
+                } else {
+                    res.status(500).json({
+                        type: "Internal Server Error",
+                        message: "Their is some error from the server side"
+                    })
+                }
+            }
+        } else {
             const result = await twilio(phoneNumber)
-            if(result == 'pending'){
+            if (result == 'pending') {
                 res.status(200).json({
                     type: "Success",
                     message: `OTP has been sent on number ${phoneNumber}`,
@@ -49,73 +64,66 @@ exports.findOne = async function(req, res){
                 })
             }
         }
-    }else{
-        const result = await twilio(phoneNumber)
-        if(result == 'pending'){
-            res.status(200).json({
-                type: "Success",
-                message: `OTP has been sent on number ${phoneNumber}`,
-                redirecting: "redirect to enter otp api to enter and verify otp </otp/verify>"
-            })
-        } else {
-            res.status(500).json({
-                type: "Internal Server Error",
-                message: "Their is some error from the server side"
-            })
-        }
-
-    }
-    }catch(err){
-        console.log(err)
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            status: "Internal Server Error",
+            message: "Their is some issue with the server"
+        })
     }
 }
-    
+
 /**
  * This funciton sends the otp with the number linked with that otp and verify it
  * @param {phoneNumber} req.body
  * @param {otp} req.body 
  */
-exports.verify = async function(req, res){
-        try{
-            const otp = req.body.otp;
-            const phoneNumber = req.body.phoneNumber;
-            const projection = { _id: 0, __v:0}
-            const isUserPresent = await User.findOne({"phoneNumber" : phoneNumber},projection);
-            const result = await verify(phoneNumber,otp);
-            if(result == "pending"){
-                res.status(401).json({
-                    type: "Unauthorized",
-                    message: "OTP entered is in-correct",
-                })
-            } 
-            const token = jwt.sign({id: phoneNumber}, "JWT_SECRET", { expiresIn: "12h"});
-            let Token = new tokenModel({
-                key: phoneNumber,
-                token : token
+exports.verify = async function (req, res) {
+    try {
+        const otp = req.body.otp;
+        const phoneNumber = req.body.phoneNumber;
+        const projection = { _id: 0, __v: 0 }
+        const isUserPresent = await User.findOne({ "phoneNumber": phoneNumber }, projection);
+        const result = await verify(phoneNumber, otp);
+        if (result == "pending") {
+            res.status(401).json({
+                type: "Unauthorized",
+                message: "OTP entered is in-correct",
             })
-            const tokendb = await tokenModel.findOne({"key" : phoneNumber},projection);
-            if(tokendb){
-                await tokenModel.findOneAndUpdate({"key" : phoneNumber}, {$set:{"token":token}})
-            }else{
-                await tokenModel.save()
-            }
-            if(isUserPresent){
-                if(result == "approved"){
-                    const userName = isUserPresent.name;
-                    res.status(200).json({
-                        type: "Success",
-                        message: `WELCOME! ${userName} ` 
-                    })
-                }
-            } else{
-                if(result == "approved"){
-                    res.status(404).json({
-                        type: "Not Found",
-                        message: "user is not registered. Please register it using api </user/reg>"
-                    })
-                }
-            }
-        }catch(error){
-            console.log(error)
         }
+        const token = jwt.sign({ id: phoneNumber }, "JWT_SECRET", { expiresIn: "12h" });
+        const Token = new tokenModel({
+            key: phoneNumber,
+            token: token
+        })
+        // TO do redis implementation for managin token more efficiently
+        const tokendb = await tokenModel.findOne({ "key": phoneNumber }, projection);
+        if (tokendb) {
+            await tokenModel.findOneAndUpdate({ "key": phoneNumber }, { $set: { "token": token } })
+        } else {
+            await tokenModel.save()
+        }
+        if (isUserPresent) {
+            if (result == "approved") {
+                const userName = isUserPresent.name;
+                res.status(200).json({
+                    type: "Success",
+                    message: `WELCOME! ${userName} `
+                })
+            }
+        } else {
+            if (result == "approved") {
+                res.status(404).json({
+                    type: "Not Found",
+                    message: "user is not registered. Please register it using api </user/reg>"
+                })
+            }
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            status: "Internal Server Error",
+            message: "Their is some issue with the server"
+        })
     }
+}
